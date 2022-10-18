382688e352acd85619b94c4ae075c8c476fc72d3
Firas Alkhatib <firas88alkhatib@gmail.com>
2022-10-18 10:31:40 +0300

    renamed app

--- src/db.rs
+++ src/db.rs
@@ -1,6 +1,6 @@
 use diesel::prelude::*;
 
 pub fn get_connection() -> PgConnection {
-    let database_url: &str = "postgres://postgres:postgres@0.0.0.0/rustdb";
+    let database_url: &str = "postgres://postgres:postgres@0.0.0.0/rustapp";
     PgConnection::establish(&database_url).expect(&format!("Erro connecting to {}", database_url))
 }
