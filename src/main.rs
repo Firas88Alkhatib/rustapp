c354f8f443fce9bf43bbc4dc7e4261f43d3d97f4
Firas Alkhatib <firas88alkhatib@gmail.com>
2022-10-20 08:39:32 +0300

    used routing in main

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,25 @@
 #[macro_use]
 extern crate diesel;
+use actix_web::{App, HttpServer};
+use std::io::Result;
+
 mod api;
 mod db;
 mod models;
+mod repos;
+mod routes;
 mod schema;
 
-use actix_web::{App, HttpServer};
-use api::user::{create_user, get_all_users, get_user, update_user};
+use crate::routes::{get_products_routes, get_users_routes};
+
+mod tests;
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> Result<()> {
     HttpServer::new(|| {
-        App::new()
-            .service(get_user)
-            .service(create_user)
-            .service(create_user)
-            .service(get_all_users)
-            .service(update_user)
+        let users_routes = get_users_routes();
+        let products_routes = get_products_routes();
+        App::new().service(users_routes).service(products_routes)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
