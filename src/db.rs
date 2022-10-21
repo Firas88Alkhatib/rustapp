e22477d45e01ac48c3f6d70637724749e09f9c10
Firas Alkhatib <firas88alkhatib@gmail.com>
2022-10-21 12:33:22 +0300

    added docker

--- src/db.rs
+++ src/db.rs
@@ -1,6 +1,7 @@
 use diesel::prelude::*;
+use std::env;
 
 pub fn get_connection() -> PgConnection {
-    let database_url: &str = "postgres://postgres:postgres@0.0.0.0/rustapp";
+    let database_url = env::var("DATABASE_URL").unwrap_or("postgres://postgres:postgres@0.0.0.0/rustapp".to_string());
     PgConnection::establish(&database_url).expect(&format!("Erro connecting to {}", database_url))
 }
