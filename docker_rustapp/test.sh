c205fc7d4f42a742615c5e310d1442e3bb417d02
Firas Alkhatib <firas88alkhatib@gmail.com>
2022-10-24 14:52:52 +0300

    updated docker compose with the products service, and added health check

--- docker_rustapp/test.sh
+++ docker_rustapp/test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
-# TODO
+
 # database migration command or rust application may run before postgres is up and ready
+# there fore  Healthcheck is implemented in compose file,
+# alternatively:
 # we can docker compose run [service] and follow log until we receive postgres log "database system is ready to accept connections"
+# or by running "pg_isready"
 # then we run the migration and the app
 docker compose -f config-test.yml up --abort-on-container-exit
