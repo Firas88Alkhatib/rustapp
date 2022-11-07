4f8019fe630096cc518e5d681036d121bc032374
Firas Alkhatib <firas88alkhatib@gmail.com>
2022-11-07 13:38:16 +0200

    improved docker compose implementation

---
+++ docker_rustapp/wait_for_db.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+db_host=$1
+db_port=$2
+
+# until nc -w 1 $db_name $db_port; do
+until [ "$(curl -s http://$db_host:$db_port; echo $?)" -eq 52 ] ; do
+    echo "Waiting for $db_host to be ready"
+    sleep 5
+done
+
+
+
--- main/src/main.rs
+++ main/src/main.rs
@@ -20,8 +20,8 @@ use crate::app::init_app;
 use actix_web::HttpServer;
 // use crate::repos::products_repo::ProductsRepo;
 // use crate::repos::users_repo::UsersRepo;
-use db::get_connection_pool;
 use config::urls::get_database_url;
+use db::get_connection_pool;
 
 // implement a struct for database and http client
 
@@ -29,7 +29,6 @@ use config::urls::get_database_url;
 async fn main() -> std::io::Result<()> {
     let connection_pool = get_connection_pool(get_database_url());
     HttpServer::new(move || {
-        init_app(connection_pool.clone())
         // let products_service_url = env::var("PRODUCTS_URL").unwrap_or(String::from("http://localhost:5001/products"));
         // let database_url = env::var("DATABASE_URL").unwrap_or(String::from("postgres://postgres:postgres@0.0.0.0/rustapp"));
 
@@ -43,6 +42,8 @@ async fn main() -> std::io::Result<()> {
         //     .app_data(products_repo)
         //     .service(public_routes)
         //     .service(protected_routes)
+        println!("Starting main service server");
+        init_app(connection_pool.clone())
     })
     .bind(("0.0.0.0", 8080))?
     .run()
